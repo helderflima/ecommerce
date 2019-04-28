e2d13b64fbeb351b1e5100462f797ef78343a7e1
Helder Lima <helder.fl@hotmail.com>
2019-04-28 17:29:19 -0300

    funcionalidade de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,10 +10,14 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/"){
+
+		//$this->defaults["data"]["session"] = $_SESSION;
 
 		$this->options = array_merge($this->defaults, $opts);
 
@@ -26,7 +30,7 @@ class Page{
 		Tpl::configure($config);
 		$this->tpl = new Tpl;
 		$this->setData($this->options["data"]);
-		$this->tpl->draw("header");
+		if ($this->options['header'] === true) $this->tpl->draw("header");
 	}
 
 
@@ -48,7 +52,8 @@ class Page{
 
 
 	public function __destruct(){
-		$this->tpl->draw("footer");
+
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
